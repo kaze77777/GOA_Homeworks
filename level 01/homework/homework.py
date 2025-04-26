adf1839d806dad4b09229813f10f68ae7e6a0069
kaze77777 <korzavkorzik@gmail.com>
2025-04-26 13:26:09 +0400

    GOA

--- level 01/homework/homework.py
+++ level 01/homework/homework.py
@@ -1,2 +1,35 @@
 print("nika gogokhia")
 print("this is day 1")
+
+
+name = "nika"
+print (name)
+
+colors = "blue","green","black"
+
+print (colors)
+
+age1 = 15
+
+age2 = 16
+
+age3 = 16
+
+print (age1, age2, age3)
+
+name1 = "nika"
+
+name2 = "mate"
+
+name3 = "qeti"
+
+name4 = "lika"
+
+name5 = "temo"
+
+print (name1,name2,name3,name4,name5)
+
+pet = "simba"
+print (pet)
+
+# string - ში ინახება სიტყვები,intejer - ში ინახება მთელი რიცხვები და float - ში ინახება ათწილადი რიცხვები
