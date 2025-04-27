02b7e68afe5ad8bfbe5e16d9111e1eeac9da92d6
kaze77777 <korzavkorzik@gmail.com>
2025-04-27 21:14:28 +0400

    GOA

--- level 03/classwork/classwork.py
+++ level 03/classwork/classwork.py
@@ -4,7 +4,13 @@ float1 = 15,5
 boolean = 15 > 10
 print(boolean)
 print(float1,integer,string)
+# print(input("enter your name: "))
+# print(input("enter your surname: "))
+# print(input("enter your age: "))
 
-print(input("enter your age: "))
 
-print (15)
+
+name = input("enter your name: ")
+surname = input("enter your surname: ")
+age = input("enter your age: ")
+print(name,surname,age)
