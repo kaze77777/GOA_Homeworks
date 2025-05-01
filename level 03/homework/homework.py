b97ec6a507f21390476a4317f5cfa39ef5191cca
kaze77777 <korzavkorzik@gmail.com>
2025-05-01 20:24:18 +0400

    GOA

---
+++ level 03/homework/homework.py
@@ -0,0 +1,38 @@
+#მომხმარებელს შემოატანინეთ მისი სახელი და შემდეგ დაპრინტეთ
+
+name = input("enter your name: ")
+print(name)
+
+#შექმენით 5-5 მაგალითი მათემატიკურ ოპერატორებზე +, -, * და /
+
+print(2 + 3)
+print(4 + 6)
+print(3 + 7)
+print(5 + 9)
+print(4 + 6)
+
+print(10 - 4)
+print(9 - 4)
+print(7 - 5)
+print(9 - 4)
+print(7 - 5)
+
+print(2 * 5)
+print(5 * 3)
+print(3 * 3)
+print(2 * 5)
+print(5 * 3)
+
+print(10 / 5)
+print(20 / 10)
+print(15 / 3)
+print(10 / 5)
+print(20 / 10)
+
+
+name= "nika"
+surname="gogokhia"
+age= 15
+country = "Georgia"
+city = "zugdidi"
+print(name,surname,age,country,city)
