5b791a315f4f274a4657fdccc77ea89ea93df647
kaze77777 <korzavkorzik@gmail.com>
2025-05-04 18:08:02 +0400

    GOA

--- level 03/classwork/classwork.py
+++ level 03/classwork/classwork.py
@@ -1,16 +1,12 @@
-string = "nika"
-integer = 15
-float1 = 15,5
-boolean = 15 > 10
-print(boolean)
-print(float1,integer,string)
-# print(input("enter your name: "))
-# print(input("enter your surname: "))
-# print(input("enter your age: "))
 
 
 
-name = input("enter your name: ")
-surname = input("enter your surname: ")
-age = input("enter your age: ")
-print(name,surname,age)
+
+name = "Alice"
+age = (25)
+height = (5.4)
+
+print("Name")
+print("Age")
+print("Height: ", height)
+print("Hello " + " Your age is 25")
---
+++ level 04/homework/homework.py
@@ -0,0 +1,20 @@
+
+name = "hello, my name is nika"
+surname = "and my surname is gogokhia, i am"
+age = 15
+ss = "years old"
+print(name,surname,age,ss)
+
+
+x = 234
+y = 342
+z = 14
+w = 43
+print(x*y)
+print(w*z+(z-w))
+print(x+z+w-y)
+print(x/w)
+
+
+
+
