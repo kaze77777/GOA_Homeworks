17ecacc7690a251ae790706b4ee7fe508d105359
kaze77777 <korzavkorzik@gmail.com>
2025-05-09 22:20:39 +0400

    GOA

---
+++ level 04/classwork/classwork.py
@@ -0,0 +1,10 @@
+name = "nika"
+surname = "gogokhia"
+age = 15
+pet = "dog"
+hobby = "football"
+
+print(f"hello my name is {name} and my surname is {surname} i am {age} years old also i have a pet {pet} and my hobby is {hobby}")
+
+
+name  = input()
---
+++ level 05/homework/homework.py
@@ -0,0 +1,15 @@
+print = input("name:")
+print = input("age:")
+
+width = input("enter number:")
+height= input("enter number:")
+s = (width)
+p = (height)
+
+print("s=(width+height)/p")
+print("p=(width+height)*2")
+
+
+
+# Input - არის ნებისმიერი რამ რასაც მომხმარებელი აკეთებს
+# Output - არის მომხმარებლის მიერ შესრულებული მოქმედება გამოტანილი ტერმინალზე
