aaf1f9b2985e227652c8b4adc17194d77ac1b441
kaze77777 <korzavkorzik@gmail.com>
2025-05-23 19:21:23 +0400

    GOA

---
+++ level 07/homework/homework.py
@@ -0,0 +1,40 @@
+#1)დაწერე პროგრამა, რომელიც while ციკლით დაბეჭდავს რიცხვებს 1-დან 10-მდე.2)დაწერე პროგრამა, რომელიც დაბეჭდავს რიცხვებს 10-დან 1-მდე.3)კომენტარებით ახსენი while loop. 4)დაწერე პროგრამა, რომელიც სთხოვს მომხმარებელს პაროლის შეყვანას. სწორი პაროლია "python123". სანამ სწორად არ შეიყვანს, მოთხოვე თავიდან.5)მომხმარებელმა უნდა შეიყვანოს რიცხვი n. პროგრამამ while ციკლით უნდა დაითვალოს 1-დან n-მდე რიცხვების ჯამი.
+
+num = 1
+while num < 10:
+    print(num)
+    num += 1
+
+
+num = 10
+while num > 0:
+    print(num)
+    num -= 1
+
+#არის ციკლი Python-ში, რომელიც ასრულებს კოდის ბლოკს მანამ, სანამ პირობა მართალია 
+
+# user_password = input("enter password: ")
+# correct_password = "python123"
+# while user_password != correct_password:
+#     print('try again')
+#     user_password = input("enter password: ")
+# print("correct")
+
+
+
+# num1 = int(input("enter num: "))
+# num2 = 0
+# while num2 != num1:
+#     print(num2)
+#     num2 += 1
+
+
+n = int(input("enter number n: "))
+i = 1
+summ = 0
+
+while i <= n:
+    summ += i
+    i += 1
+
+print("sum:", summ)
---
+++ level 08/homework/homework.py
@@ -0,0 +1,11 @@
+for i in range(1, 10):
+    print(i)
+
+for i in range(5):
+    print("hello world")
+
+
+for i in range(0, 10, 2):
+    print(i)
+
+#for loop-არის ციკლი, რომელიც იმეორებს კოდს ელემენტების სიის ან დათვლილი რიცხვების მიხედვით.
